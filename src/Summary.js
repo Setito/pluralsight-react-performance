7b1204c6201faf1ea96ac1d03af76ec6b328390d
Maximiliano Chereza <maximiliano.chereza@sandfield.co.nz>
2020-12-18 12:39:21 +1300

    Adding info to Take Aways

--- src/Summary.js
+++ src/Summary.js
@@ -1,16 +1,9 @@
 import React from "react";
 import levenshtein from "levenshtein";
 
-export class Summary extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    const oldKeys = Object.keys(this.props.cards);
-    const newKeys = Object.keys(nextProps.cards);
-
-    return oldKeys.length !== newKeys.length;
-  }
-
-  render() {
-    const cards = Object.values(this.props.cards);
+export const Summary = React.memo(
+  function Summary(props) {
+    const cards = Object.values(props.cards);
 
     const distances = { max: 0, min: 100000 };
     cards.forEach((currentCard) => {
@@ -36,10 +29,19 @@ export class Summary extends React.Component {
           border: "3px solid #333",
         }}
       >
-        <div>You have {Object.keys(this.props.cards).length} cards!</div>
+        <div>You have {Object.keys(props.cards).length} cards!</div>
         <div>Max difference in labels: {distances.max}</div>
         <div>Min difference in labels: {distances.min}</div>
       </div>
     );
-  }
+  },
+  (p1, p2) => Object.keys(p1.cards).length === Object.keys(p2.cards).length
+);
+
+/*shouldComponentUpdate(nextProps) {
+  const oldKeys = Object.keys(this.props.cards);
+  const newKeys = Object.keys(nextProps.cards);
+
+  return oldKeys.length !== newKeys.length;
 }
+*/
